bd7bee5de6cfc3f9494d3ddc05b2f1285828436a
Jay McGuinness <jay@bigjump.co.uk>
2019-09-22 06:17:38 +0100

    mac

--- src/pages/Results.vue
+++ src/pages/Results.vue
@@ -1,5 +1,6 @@
 <template>
   <q-page class="flex flex-center">
+    {{o}}
 
     <q-btn
       rounded
@@ -12,10 +13,25 @@
 <script>
 export default {
   name: "PageIndex",
+  mounted() {
+    // Promise.all([
+    //   navigation.permissions.request({ name: "accelerometer" }),
+    //   navigation.permissions.request({ name: "gyroscope" })
+    // ]).then(results => {
+    //   //  ...
+    // });
+
+    window.addEventListener("deviceorientation", this.deviceOrientation, true);
+  },
   data() {
     return {
-      model: "one"
+      o: { alpha: 0, beta: 0, gamma: 0 }
     };
+  },
+  methods: {
+    deviceOrientation(e) {
+      this.o = e;
+    }
   }
 };
 </script>
--- src/pages/Scan.vue
+++ src/pages/Scan.vue
@@ -1,11 +1,21 @@
 <template>
   <q-page class="flex flex-center">
+    <div
+      v-for="(f,index) in frames"
+      :key="index"
+    >
+      <img :src="frames[index].fullPath" />
 
+      {{frames[index].fullPath}}
+    </div>
     <q-btn
       rounded
       outline
       color="primary"
+      @click="capture"
     >Scanner</q-btn>
+
+    <q-btn @click="stop">Stop</q-btn>
   </q-page>
 </template>
 
@@ -14,8 +24,33 @@ export default {
   name: "PageIndex",
   data() {
     return {
-      model: "one"
+      frames: []
     };
+  },
+  mounted() {
+    //   alert("mounted");
+  },
+  methods: {
+    capture() {
+      let options = {
+        x: 0,
+        y: 0,
+        width: window.screen.width,
+        height: window.screen.height,
+        camera: CameraPreview.CAMERA_DIRECTION.FRONT,
+        toBack: true,
+        tapPhoto: true,
+        tapFocus: false,
+        previewDrag: false,
+        storeToFile: false,
+        disableExifHeaderStripping: false
+      };
+
+      CameraPreview.startCamera(options);
+    },
+    stop() {
+      CameraPreview.stopCamera();
+    }
   }
 };
 </script>
