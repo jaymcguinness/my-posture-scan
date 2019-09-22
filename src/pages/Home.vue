aca2a189af02e3652c366a0083885fdeb2d5cc92
Jay McGuinness <jay@bigjump.co.uk>
2019-09-22 07:26:23 +0100

    home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <q-page class="flex flex-center">
-    <q-input label="Height" />
+    <q-card flat v-for="(image,index) in images" :key="index">
+      <q-card-section>
+        <img :src="image" />
+      </q-card-section>
+      <q-card-section>ssss</q-card-section>
+    </q-card>
+    <!-- <q-input label="Height" />
     <q-btn-toggle
       v-model="model"
       class="my-custom-toggle"
@@ -14,17 +20,34 @@
           {label: 'cm', value: 'one'},
           {label: 'ft & inches', value: 'two'}
         ]"
-    />
-    <q-btn rounded outline color="primary">Next</q-btn>
+    />-->
+
+    <!-- <q-btn rounded outline color="primary">Next</q-btn> -->
   </q-page>
 </template>
+<style scoped>
+img {
+  -webkit-filter: grayscale(50%); /* Safari 6.0 - 9.0 */
+  filter: grayscale(50%);
+  width: 200px;
+  height: 140px;
+  border-radius: 20px;
+}
 
+.q-card-section {
+  padding: 0px;
+}
+</style>
 <script>
 export default {
   name: "PageIndex",
   data() {
     return {
-      model: "one"
+      images: [
+        "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/topic_centers/2018-7/posture_exercises-1296x728-body2.jpg?w=1155&h=1528",
+        "https://images-prod.healthline.com/hlcmsresource/images/topic_centers/2018-7/posture_exercises-1296x728-body8.jpg",
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/runners-calf-stretch-on-wall-1568388199.jpg?resize=480:*"
+      ]
     };
   }
 };
--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -1,25 +1,30 @@
 <template>
-  <q-page
-    class="flex column flex-center"
-    style="width:100%"
-  >
-    <div>
-      <img
-        src="statics/square.png"
-        width="50%"
-      />
-    </div>
+  <q-page class="flex column flex-center" style="width:100%;background-color:#D92176;color:white">
+    <h6>SIGN UP</h6>
 
-    <div>
-      <q-input label="Name"></q-input>
-    </div>
-    <div>
-      <q-input label="Height"></q-input>
-    </div>
+    <q-btn color="blue">Sign up with Facebook</q-btn>
+    <q-btn color="red">Sign up with Google</q-btn>
+    <q-btn color="orange">Sign up with Instagram</q-btn>
+    <q-btn color="grey">Sign up with Apple</q-btn>OR
+    <q-input label="First name"></q-input>
+    <q-input label="Surname"></q-input>
+    <q-input label="Email"></q-input>
+    <q-input label="Password"></q-input>
+
+    <q-checkbox>Terms</q-checkbox>
+
+    <q-input label="Height"></q-input>
+
     <a @click="$router.go(-1)">BACK</a>
   </q-page>
 </template>
-
+<style scoped>
+.q-input {
+  background-color: white;
+  padding: 10px;
+  width: 100%;
+}
+</style>
 <script>
 export default {
   name: "Welcome",
